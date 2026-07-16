5f1b74569e66f61f14bbf907f6af94911daccc47
峰哥 <362053534@qq.com>
2026-07-16 19:03:54 +0800

    尝试添加BDMHDD实现

--- __usbd_bd_assault/src/irx_imports.h
+++ __usbd_bd_assault/src/irx_imports.h
@@ -16,13 +16,7 @@
 #include "irx.h"
 
 /* Please keep these in alphabetical order!  */
-#include <bdm.h>
-#include <dev9.h>
-#include "loadcore.h"
 #include <modload.h>
 #include "stdio.h"
-#include "sysclib.h"
-#include "thbase.h"
-#include "thevent.h"
 
 #endif /* IOP_IRX_IMPORTS_H */
--- __usbd_bd_assault/src/usbd/usbd_usbd.c
+++ __usbd_bd_assault/src/usbd/usbd_usbd.c
@@ -1,8 +1,8 @@
 /*
  * BDM HDD driver entry point.
  *
- * DEV9 is embedded into this module so POPStarter can load one driver file.
- * After DEV9 is available, ATAD registers the ATA disk as a BDM block device.
+ * DEV9 and the PS2SDK's BDM-enabled ATAD are embedded into this module so
+ * POPStarter can load one driver file.
  */
 
 #include <irx.h>
@@ -10,8 +10,11 @@
 #include <modload.h>
 #include <stdio.h>
 
+#define MODNAME "bdm_hdd"
+IRX_ID(MODNAME, 1, 1);
+
 extern unsigned char bdm_hdd_dev9_irx[];
-extern int bdm_hdd_atad_start(int argc, char *argv[]);
+extern unsigned char bdm_hdd_atad_irx[];
 
 int _start(int argc, char *argv[])
 {
@@ -24,6 +27,13 @@ int _start(int argc, char *argv[])
         return MODULE_NO_RESIDENT_END;
     }
 
-    return bdm_hdd_atad_start(argc, argv);
+    printf("BDM HDD: loading embedded ATA BDM driver\n");
+    result = LoadModuleBuffer(bdm_hdd_atad_irx);
+    if (result < 0) {
+        printf("BDM HDD: ATA BDM load failed: %d\n", result);
+        return MODULE_NO_RESIDENT_END;
+    }
+
+    return MODULE_RESIDENT_END;
 }
 
