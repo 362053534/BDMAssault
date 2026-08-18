524236a4b271eee58cbc435984deca9833d28ab6
峰哥 <362053534@qq.com>
2026-08-18 15:00:58 +0800

    USB线程栈提高版本

--- __usbd_bd_assault/src/usbd/usbd_driver.c
+++ __usbd_bd_assault/src/usbd/usbd_driver.c
@@ -271,7 +271,7 @@ int initCallbackThread(void)
     thread.option    = 0;
     thread.thread    = callbackThreadFunc;
 #ifndef MINI_DRIVER
-    thread.stacksize = 0x4000; // 16KiB
+    thread.stacksize = 0x8000; // 栈大小 32KiB
 #else
     thread.stacksize = 0x0800; //  2KiB
 #endif
--- __usbd_bd_assault/src/usbd/usbd_hcd.c
+++ __usbd_bd_assault/src/usbd/usbd_hcd.c
@@ -520,7 +520,7 @@ int hcdInit(void)
     thread.option    = 0;
     thread.thread    = hcdIrqThread;
 #ifndef MINI_DRIVER
-    thread.stacksize = 0x4000; // 16KiB
+    thread.stacksize = 0x8000; // 栈大小 32KiB
 #else
     thread.stacksize = 0x0800; //  2KiB
 #endif
