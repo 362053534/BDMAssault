801fb42ca5bd766aeb10af39e50a6d3c167f3c7b
峰哥 <362053534@qq.com>
2026-08-20 20:03:43 +0800

    尝试修复SMB启动错误

--- SMB/src/irx_imports.h
+++ SMB/src/irx_imports.h
@@ -24,10 +24,10 @@
 #include <thbase.h>
 #include <thsemap.h>
 
-// 此驱动与OPL的兼容网络栈成对使用，不继承构建SDK中可能更高的导入版本。
+// 使用PS2SDK 2.4兼容接口，同时兼容POPStarter现有的ps2smap驱动。
 #undef ps2ip_IMPORTS_start
 #undef ps2ip_IMPORTS_end
-#define ps2ip_IMPORTS_start DECLARE_IMPORT_TABLE(ps2ip, 1, 3)
+#define ps2ip_IMPORTS_start DECLARE_IMPORT_TABLE(ps2ip, 2, 4)
 #define ps2ip_IMPORTS_end   END_IMPORT_TABLE
 
 int lwip_recvsplit(int s, void *header, int index, void *payload, int plen, unsigned int flags);
